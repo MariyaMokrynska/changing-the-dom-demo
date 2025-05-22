a3edd7f5173d9d5961d0a9c90a6a5969f26abbeb
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-05-21 22:41:02 -0700

    done

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ appearanceHeading.textContent = 'Qualities and Traits of a Crab';
 // Changing Appearance//
 ////////////////////////
 
-// const importantFact = document.getElementById('facts__fact--important');
+const importantFact = document.getElementById('facts__fact--important');
 
-// importantFact.className = `${importantFact.className} highlight`;
+importantFact.className = `${importantFact.className} highlight`;
 
 ///////////////////////////
 // Creating a New Element//
 ///////////////////////////
 
-// const newAppearanceFact = document.createElement('li');
+const newAppearanceFact = document.createElement('li');
 
-// newAppearanceFact.textContent = 'covered in sand (when on a sandy beach)';
+newAppearanceFact.textContent = 'covered in sand (when on a sandy beach)';
 
-// const appearanceList = document.getElementById('facts__list');
+const appearanceList = document.getElementById('facts__list');
 
-// appearanceList.appendChild(newAppearanceFact);
+appearanceList.appendChild(newAppearanceFact);
